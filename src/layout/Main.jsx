24aedebd00002acf02f968e373ecb8aec0accf08
Md Jahid Hasan <jahidhasan658560@gmail.com>
2024-06-28 20:19:17 +0600

    Header component and footer component added

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import Footer from "../pages/Home/Home/shared/Footer/Footer";
+import Header from "../pages/Home/Home/shared/Header/Header";
+
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/Home/Home/shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <p className="text-center">copyright of my news</p>
+        </div>
+    );
+};
+
+export default Footer;
