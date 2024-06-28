1328a525aeb6259a1c8f1a26b7fd78ef24305115
Md Jahid Hasan <jahidhasan658560@gmail.com>
2024-06-29 01:18:47 +0600

    express js added

--- src/pages/Home/Home/shared/LeftNav/LeftNav.jsx
+++ src/pages/Home/Home/shared/LeftNav/LeftNav.jsx
@@ -1,6 +1,14 @@
 
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/categories')
+        .then(res =>res.json())
+        .then(data=> console.log(data))
+        .catch(error =>console.error(error))
+    },[])
     return (
         <div>
             <h2>Left Nav</h2>
--- src/pages/Home/Home/shared/RightNav/RightNav.jsx
+++ src/pages/Home/Home/shared/RightNav/RightNav.jsx
@@ -1,11 +1,36 @@
-
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import QZone from "../QZone/QZone";
+import bg from '../../../../../assets/bg.png'
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <h4>Login with</h4>
+        <Button className="mb-2" variant="primary">
+          <FaGoogle />
+          Login with Google
+        </Button>
+        <Button variant="secondary">
+          <FaGithub />
+          Login with Github
+        </Button>
+      </div>
+      <div>
+        <h4>Find Us On</h4>
+        <ListGroup>
+          <ListGroup.Item disabled><FaFacebook />Facebook</ListGroup.Item>
+          <ListGroup.Item><FaTwitter />Twitter</ListGroup.Item>
+          <ListGroup.Item><FaInstagram />Instagram</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div>
+        <img src={bg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default RightNav;
