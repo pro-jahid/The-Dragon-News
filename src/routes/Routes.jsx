862b9c1202c9c8ff569bf332c20ab97c3b645378
Md Jahid Hasan <jahidhasan658560@gmail.com>
2024-09-04 03:58:18 +0600

    vercel added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -10,50 +10,56 @@ import PrivateRoute from "./PrivateRoute/PrivateRoute";
 import Turms from "../pages/Home/Home/shared/Turms/Turms";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <LoginLayout />,
-        children: [
-            {
-                path: '/',
-                element: <Navigate to="/category/0" />
-            },
-            {
-                path: '/login',
-                element: <Login />
-            },
-            {
-                path: 'register',
-                element: <Register />
-            },
-            {
-                path:'terms',
-                element:<Turms></Turms>
-            }
-        ]
-    },
-    {
-        path: 'category',
-        element: <Main />,
-        children: [
-            {
-                path: '/category/:id',
-                element: <Category />,
-                loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`)
-            }
-        ]
-    },
-    {
-        path: 'news',
-        element: <Newslayout />,
-        children: [
-            {
-                path: '/news/:id',
-                element: <PrivateRoute><News /></PrivateRoute>,
-                loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
-            }
-        ]
-    }
+  {
+    path: "/",
+    element: <LoginLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/category/0" />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "terms",
+        element: <Turms></Turms>,
+      },
+    ],
+  },
+  {
+    path: "category",
+    element: <Main />,
+    children: [
+      {
+        path: "/category/:id",
+        element: <Category />,
+        loader: ({ params }) =>
+          fetch(`https://the-dragon-news-server-sandy.vercel.app/categories/${params.id}`),
+      },
+    ],
+  },
+  {
+    path: "news",
+    element: <Newslayout />,
+    children: [
+      {
+        path: "/news/:id",
+        element: (
+          <PrivateRoute>
+            <News />
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`https://the-dragon-news-server-sandy.vercel.app/news/${params.id}`),
+      },
+    ],
+  },
 ]);
 
 export default router;
